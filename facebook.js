d27d551131d5b3fe2b6bc45531bc7bdbef0c0abf
joshMilano <giorgio.seregni@gmail.com>
2016-02-08 16:08:03 +0100

    Create facebook.js

---
+++ facebook.js
@@ -0,0 +1,12 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$( document ).ready(function() {
+var html = " <div id="fb-root"></div><div class="fb-like" data-href="https://www.facebook.com/Guesthouse-Web-1679657215585362/" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>";
+$('body').append(html); 
+});
